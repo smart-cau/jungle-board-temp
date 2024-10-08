fe4879f625624fd14cc2da7435dcabb5e47d7eb7
HyeonWooJung <1995hyunwoo@gmail.com>
2024-10-08 21:50:50 +0900

    feat: config package로 민감 정보 관리

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,14 +1,18 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Board } from '../boards/entity/board.entity';
 import { User } from '../auth/entity/user.entity';
+import * as config from 'config';
+import { DbConfig } from '../../types/config.types';
+
+const dbConfig: DbConfig = config.get('db');
 
 export const TypeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'board-app',
+  type: dbConfig.type,
+  host: process.env.RDS_HOSTNAME || dbConfig.host,
+  port: +process.env.RDS_PORT || dbConfig.port,
+  username: process.env.RDS_USERNAME || dbConfig.username,
+  password: process.env.RDS_PASSWORD || dbConfig.password,
+  database: process.env.RDS_DATABASE || dbConfig.database,
   entities: [Board, User],
-  synchronize: true,
+  synchronize: dbConfig.synchronize,
 };
